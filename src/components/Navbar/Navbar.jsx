636f74e7b8170cf723874a688d2ccaf60ae3955e
daniikhan23 <daniyal.ashraf.2021@gmail.com>
2024-11-18 22:24:49 +0500

    Updated navbar elements effects on hover and active

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,223 @@ import {
 } from "../../lib/chakra-components/ui/menu";
 import LumineLogo from "../../assets/navbar/logo.png";
 import MenuIcon from "../../assets/navbar/menu-dropdown-icon.png";
+import BlackMenuIcon from "../../assets/navbar/menu-dropdown-icon-black.png";
 import "../../styles/fonts.css";
+import { useState } from "react";
+
+const Menu = () => {
+  return (
+    <>
+      <HStack
+        justifyContent={"center"}
+        alignItems={"center"}
+        gap={"20px"}
+        height={"100%"}
+      >
+        {/* People */}
+        <MenuRoot>
+          <MenuTrigger asChild>
+            <Button
+              backgroundColor="inherit"
+              color="white"
+              size="sm"
+              fontSize="18px"
+              fontWeight={500}
+              height="100%"
+              borderRadius="0px"
+              position="relative"
+              _hover={{
+                color: "black",
+                backgroundColor: "white",
+              }}
+              css={{
+                "&[data-state=open]": {
+                  color: "black",
+                  backgroundColor: "white",
+                },
+                "& img:last-child": {
+                  display: "none",
+                },
+                "&:hover img:first-child, &[data-state=open] img:first-child": {
+                  display: "none",
+                },
+                "&:hover img:last-child, &[data-state=open] img:last-child": {
+                  display: "block",
+                },
+              }}
+            >
+              People
+              <Image src={MenuIcon} />
+              <Image src={BlackMenuIcon} />
+            </Button>
+          </MenuTrigger>
+          <MenuContent>
+            <MenuItem value="new-txt-a">New Text File</MenuItem>
+            <MenuItem value="new-file-a">New File...</MenuItem>
+            <MenuItem value="new-win-a">New Window</MenuItem>
+            <MenuItem value="open-file-a">Open File...</MenuItem>
+            <MenuItem value="export-a">Export</MenuItem>
+          </MenuContent>
+        </MenuRoot>
+        {/* Services */}
+        <MenuRoot>
+          <MenuTrigger asChild>
+            <Button
+              backgroundColor="inherit"
+              color="white"
+              size="sm"
+              fontSize="18px"
+              fontWeight={500}
+              height="100%"
+              borderRadius="0px"
+              position="relative"
+              _hover={{
+                color: "black",
+                backgroundColor: "white",
+              }}
+              css={{
+                "&[data-state=open]": {
+                  color: "black",
+                  backgroundColor: "white",
+                },
+                "& img:last-child": {
+                  display: "none",
+                },
+                "&:hover img:first-child, &[data-state=open] img:first-child": {
+                  display: "none",
+                },
+                "&:hover img:last-child, &[data-state=open] img:last-child": {
+                  display: "block",
+                },
+              }}
+            >
+              People
+              <Image src={MenuIcon} />
+              <Image src={BlackMenuIcon} />
+            </Button>
+          </MenuTrigger>
+          <MenuContent
+            // border={"0.5px solid #FFFFFF"}
+            borderRadius={"8px"}
+            width={"200px"}
+          >
+            <MenuItem
+              value="immigration-services"
+              cursor={"pointer"}
+              fontSize={"12px"}
+              fontWeight={500}
+              _hover={{
+                color: "#BEAB7C",
+              }}
+            >
+              Immigration Services
+            </MenuItem>
+            <MenuItem
+              value="property"
+              cursor={"pointer"}
+              fontSize={"12px"}
+              fontWeight={500}
+              _hover={{
+                color: "#BEAB7C",
+              }}
+            >
+              Residential & Commercial Property{" "}
+            </MenuItem>
+            <MenuItem
+              value="res-litigation"
+              cursor={"pointer"}
+              fontSize={"12px"}
+              fontWeight={500}
+              _hover={{
+                color: "#BEAB7C",
+              }}
+            >
+              Dispute Resolution & Civil Litigation{" "}
+            </MenuItem>
+            <MenuItem
+              value="disputes"
+              cursor={"pointer"}
+              fontSize={"12px"}
+              fontWeight={500}
+              _hover={{
+                color: "#BEAB7C",
+              }}
+            >
+              Landlord & Tenant Disputes{" "}
+            </MenuItem>
+            <MenuItem
+              value="fam-children"
+              cursor={"pointer"}
+              fontSize={"12px"}
+              fontWeight={500}
+              _hover={{
+                color: "#BEAB7C",
+              }}
+            >
+              Family & Children
+            </MenuItem>
+            <MenuItem
+              value="injury"
+              cursor={"pointer"}
+              fontSize={"12px"}
+              fontWeight={500}
+              _hover={{
+                color: "#BEAB7C",
+              }}
+            >
+              Personal Injury
+            </MenuItem>
+          </MenuContent>
+        </MenuRoot>
+        {/* Menu */}
+        <MenuRoot>
+          <MenuTrigger asChild>
+            <Button
+              backgroundColor={"inherit"}
+              color={"white"}
+              size="sm"
+              fontSize={"18px"}
+              fontWeight={500}
+              height={"100%"}
+              borderRadius="0px"
+              position="relative"
+              _hover={{
+                color: "black",
+                backgroundColor: "white",
+              }}
+              css={{
+                "&[data-state=open]": {
+                  color: "black",
+                  backgroundColor: "white",
+                },
+                "& img:last-child": {
+                  display: "none",
+                },
+                "&:hover img:first-child, &[data-state=open] img:first-child": {
+                  display: "none",
+                },
+                "&:hover img:last-child, &[data-state=open] img:last-child": {
+                  display: "block",
+                },
+              }}
+            >
+              Menu
+              <Image src={MenuIcon} />
+              <Image src={BlackMenuIcon} />
+            </Button>
+          </MenuTrigger>
+          <MenuContent>
+            <MenuItem value="new-txt-a">New Text File</MenuItem>
+            <MenuItem value="new-file-a">New File...</MenuItem>
+            <MenuItem value="new-win-a">New Window</MenuItem>
+            <MenuItem value="open-file-a">Open File...</MenuItem>
+            <MenuItem value="export-a">Export</MenuItem>
+          </MenuContent>
+        </MenuRoot>
+      </HStack>
+    </>
+  );
+};
 
 const Navbar = () => {
   return (
@@ -20,11 +236,10 @@ const Navbar = () => {
         justifyContent={"space-between"}
         alignItems={"center"}
         px={"20px"}
-        py={"5px"}
         style={{ fontFamily: "CeraRoundPro" }}
       >
         {/* Logo + Title */}
-        <GridItem>
+        <GridItem py={"5px"}>
           <HStack justifyContent={"flex-start"} alignItems={"center"}>
             <Image src={LumineLogo} alt="" height={"50px"} width={"50px"} />
             <Text fontSize={"24px"} fontWeight={500}>
@@ -34,158 +249,12 @@ const Navbar = () => {
         </GridItem>
 
         {/* Menu Options */}
-        <GridItem>
-          <HStack justifyContent={"center"} alignItems={"center"} gap={"20px"}>
-            {/* People */}
-            <MenuRoot>
-              <MenuTrigger asChild>
-                <Button
-                  backgroundColor="inherit"
-                  color="white"
-                  size="sm"
-                  fontSize="18px"
-                  fontWeight={500}
-                  _hover={{
-                    color: "#BEAB7C",
-                  }}
-                  css={{ "&[data-state=open]": { color: "#BEAB7C" } }}
-                >
-                  People
-                  <Image src={MenuIcon} />
-                </Button>
-              </MenuTrigger>
-              <MenuContent>
-                <MenuItem value="new-txt-a">New Text File</MenuItem>
-                <MenuItem value="new-file-a">New File...</MenuItem>
-                <MenuItem value="new-win-a">New Window</MenuItem>
-                <MenuItem value="open-file-a">Open File...</MenuItem>
-                <MenuItem value="export-a">Export</MenuItem>
-              </MenuContent>
-            </MenuRoot>
-            {/* Services */}
-            <MenuRoot>
-              <MenuTrigger asChild>
-                <Button
-                  backgroundColor={"inherit"}
-                  color={"white"}
-                  size="sm"
-                  fontSize={"18px"}
-                  fontWeight={500}
-                  _hover={{
-                    color: "#BEAB7C",
-                  }}
-                  css={{ "&[data-state=open]": { color: "#BEAB7C" } }}
-                >
-                  Services
-                  <Image src={MenuIcon} />
-                </Button>
-              </MenuTrigger>
-              <MenuContent
-                // border={"0.5px solid #FFFFFF"}
-                borderRadius={"8px"}
-                width={"200px"}
-              >
-                <MenuItem
-                  value="immigration-services"
-                  cursor={"pointer"}
-                  fontSize={"12px"}
-                  fontWeight={500}
-                  _hover={{
-                    color: "#BEAB7C",
-                  }}
-                >
-                  Immigration Services
-                </MenuItem>
-                <MenuItem
-                  value="property"
-                  cursor={"pointer"}
-                  fontSize={"12px"}
-                  fontWeight={500}
-                  _hover={{
-                    color: "#BEAB7C",
-                  }}
-                >
-                  Residential & Commercial Property{" "}
-                </MenuItem>
-                <MenuItem
-                  value="res-litigation"
-                  cursor={"pointer"}
-                  fontSize={"12px"}
-                  fontWeight={500}
-                  _hover={{
-                    color: "#BEAB7C",
-                  }}
-                >
-                  Dispute Resolution & Civil Litigation{" "}
-                </MenuItem>
-                <MenuItem
-                  value="disputes"
-                  cursor={"pointer"}
-                  fontSize={"12px"}
-                  fontWeight={500}
-                  _hover={{
-                    color: "#BEAB7C",
-                  }}
-                >
-                  Landlord & Tenant Disputes{" "}
-                </MenuItem>
-                <MenuItem
-                  value="fam-children"
-                  cursor={"pointer"}
-                  fontSize={"12px"}
-                  fontWeight={500}
-                  _hover={{
-                    color: "#BEAB7C",
-                  }}
-                >
-                  Family & Children
-                </MenuItem>
-                <MenuItem
-                  value="injury"
-                  cursor={"pointer"}
-                  fontSize={"12px"}
-                  fontWeight={500}
-                  _hover={{
-                    color: "#BEAB7C",
-                  }}
-                >
-                  Personal Injury
-                </MenuItem>
-              </MenuContent>
-            </MenuRoot>
-            {/* Menu */}
-            <MenuRoot>
-              <MenuTrigger asChild>
-                <Button
-                  backgroundColor={"inherit"}
-                  color={"white"}
-                  size="sm"
-                  fontSize={"18px"}
-                  fontWeight={500}
-                  _hover={{
-                    color: "#BEAB7C",
-                  }}
-                  css={{
-                    "&[data-state=open]": { color: "#BEAB7C" },
-                  }}
-                >
-                  Menu
-                  <Image src={MenuIcon} />
-                </Button>
-              </MenuTrigger>
-              <MenuContent>
-                <MenuItem value="new-txt-a">New Text File</MenuItem>
-                <MenuItem value="new-file-a">New File...</MenuItem>
-                <MenuItem value="new-win-a">New Window</MenuItem>
-                <MenuItem value="open-file-a">Open File...</MenuItem>
-                <MenuItem value="export-a">Export</MenuItem>
-              </MenuContent>
-            </MenuRoot>
-          </HStack>
+        <GridItem height={"100%"}>
+          <Menu />
         </GridItem>
 
         {/* Contact Button */}
-        <GridItem justifySelf={"end"}>
+        <GridItem justifySelf={"end"} py={"5px"}>
           <Button
             fontWeight={400}
             fontSize={"15px"}
