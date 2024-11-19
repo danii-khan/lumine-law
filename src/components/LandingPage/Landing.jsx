19f30826dd1a7fb56b9aa56ed69f02b1791778ec
daniikhan23 <daniyal.ashraf.2021@gmail.com>
2024-11-19 22:01:53 +0500

    Updates to landing page, including services and panels page

--- src/components/LandingPage/Landing.jsx
+++ src/components/LandingPage/Landing.jsx
@@ -1,7 +1,28 @@
-import { Container, Box, Text, VStack, Button, HStack } from "@chakra-ui/react";
+import {
+  Container,
+  Box,
+  Text,
+  VStack,
+  Button,
+  HStack,
+  Grid,
+  GridItem,
+  Image,
+} from "@chakra-ui/react";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 import MainBg from "../../assets/landing/main-bg.jpg";
+import AboutBg from "../../assets/landing/about-bg.png";
+import ServicesBgOne from "../../assets/landing/services-bg-one.png";
+import ServicesBgTwo from "../../assets/landing/services-bg-two.png";
+import ServicesBgThree from "../../assets/landing/services-bg-three.png";
+import ServicesBgFour from "../../assets/landing/services-bg-four.png";
+import ServicesBgFive from "../../assets/landing/services-bg-five.png";
+import ServicesBgSix from "../../assets/landing/services-bg-six.png";
+import PanelOne from "../../assets/landing/panel-one.png";
+import PanelTwo from "../../assets/landing/panel-two.png";
+import PanelThree from "../../assets/landing/panel-three.png";
+import PanelFour from "../../assets/landing/panel-four.png";
 import "../../styles/fonts.css";
 import { useEffect, useState } from "react";
 
@@ -57,7 +78,7 @@ const ScrollSpySidebar = () => {
                   // fontFamily="CeraRoundPro"
                   fontWeight={400}
                   fontStyle={"italic"}
-                  fontSize={activeSection === id ? "40px" : "20px"}
+                  fontSize={activeSection === id ? "30px" : "15px"}
                   mb={activeSection === id ? "100%" : "0"}
                   opacity={activeSection === id ? "100%" : "25%"}
                 >
@@ -81,16 +102,31 @@ const ScrollSpySidebar = () => {
 
 const LandingPage = () => {
   return (
-    <Container width="100vw" maxWidth="100%" overflow="hidden" m={0} p={0}>
+    <Container
+      width="100vw"
+      maxWidth="100%"
+      overflow="hidden"
+      m={0}
+      p={0}
+      background="linear-gradient(0deg, 
+    #000000 12.5%, 
+    #124F66 35.68%, 
+    #249ECB 47.23%, 
+    #124F66 58.12%, 
+    #1B7798 67%, 
+    #000000 93%
+    )"
+    >
       <Navbar />
       {/* Hero Section */}
       <section id="none">
         <Box
           width="100%"
-          height="90vh"
+          height="100vh"
           p={0}
           m={0}
           bgImage={`url(${MainBg})`}
+          bgPos={"center"}
           bgSize={"cover"}
           bgRepeat={"no-repeat"}
           display={"flex"}
@@ -159,31 +195,357 @@ const LandingPage = () => {
       <ScrollSpySidebar />
 
       <section id="Who-we-are">
-        <Box minHeight="100vh" bg="gray.900" p={10}>
-          <Text color="white" fontSize="2xl">
-            Who We Are Content
-          </Text>
+        <Box
+          width="100%"
+          height="100vh"
+          p={0}
+          m={0}
+          bgImage={`url(${AboutBg})`}
+          bgPos={"center"}
+          bgSize={"cover"}
+          bgRepeat={"no-repeat"}
+          display={"flex"}
+          alignItems={"center"}
+        >
+          <VStack
+            justifyContent={"flex-start"}
+            alignItems={"flex-start"}
+            width={"50%"}
+            ml={"5%"}
+            gap={"20px"}
+          >
+            <Text
+              fontSize={"20px"}
+              fontWeight={300}
+              fontFamily="HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif"
+              textAlign={"left"}
+            >
+              A whole paragraph, in further details explaining what Lumine Law
+              does. Go into details. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua.
+            </Text>
+            <Text
+              fontSize={"20px"}
+              fontWeight={300}
+              fontFamily="HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif"
+              textAlign={"left"}
+            >
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit.
+            </Text>
+          </VStack>
         </Box>
       </section>
 
       <section id="Our-services">
-        <Box minHeight="100vh" bg="gray.800" p={10}>
-          <Text color="white" fontSize="2xl">
-            Our Services Content
-          </Text>
+        <Box
+          width="100%"
+          height="90vh"
+          p={0}
+          m={0}
+          display={"flex"}
+          flexFlow={"column"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          gap={"50px"}
+        >
+          <Grid
+            templateColumns="repeat(3, 1fr)"
+            templateRows={"repeat(2, 1fr)"}
+            gap="10"
+          >
+            <GridItem
+              bgImage={`url(${ServicesBgOne})`}
+              bgPos={"center"}
+              bgSize={"cover"}
+              bgRepeat={"no-repeat"}
+              height={"200px"}
+              width={"350px"}
+              borderRadius={"10px"}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <Text
+                fontSize={"35px"}
+                fontWeight={500}
+                fontFamily={"CeraRoundPro"}
+                width={"80%"}
+              >
+                Immigration Services
+              </Text>
+            </GridItem>
+            <GridItem
+              bgImage={`url(${ServicesBgTwo})`}
+              bgPos={"center"}
+              bgSize={"cover"}
+              bgRepeat={"no-repeat"}
+              height={"200px"}
+              width={"350px"}
+              borderRadius={"10px"}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <Text
+                fontSize={"35px"}
+                fontWeight={500}
+                fontFamily={"CeraRoundPro"}
+                width={"80%"}
+              >
+                Residential and Commercial Property{" "}
+              </Text>
+            </GridItem>
+            <GridItem
+              bgImage={`url(${ServicesBgThree})`}
+              bgPos={"center"}
+              bgSize={"cover"}
+              bgRepeat={"no-repeat"}
+              height={"200px"}
+              width={"350px"}
+              borderRadius={"10px"}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <Text
+                fontSize={"35px"}
+                fontWeight={500}
+                fontFamily={"CeraRoundPro"}
+                width={"80%"}
+              >
+                Dispute Resolution and Civil Litigation{" "}
+              </Text>
+            </GridItem>
+            <GridItem
+              bgImage={`url(${ServicesBgFour})`}
+              bgPos={"center"}
+              bgSize={"cover"}
+              bgRepeat={"no-repeat"}
+              height={"200px"}
+              width={"350px"}
+              borderRadius={"10px"}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <Text
+                fontSize={"35px"}
+                fontWeight={500}
+                fontFamily={"CeraRoundPro"}
+                width={"80%"}
+              >
+                Landlord and Tenant Disputes{" "}
+              </Text>
+            </GridItem>
+            <GridItem
+              bgImage={`url(${ServicesBgFive})`}
+              bgPos={"center"}
+              bgSize={"cover"}
+              bgRepeat={"no-repeat"}
+              height={"200px"}
+              width={"350px"}
+              borderRadius={"10px"}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <Text
+                fontSize={"35px"}
+                fontWeight={500}
+                fontFamily={"CeraRoundPro"}
+                width={"80%"}
+              >
+                Family and Children{" "}
+              </Text>
+            </GridItem>
+            <GridItem
+              bgImage={`url(${ServicesBgSix})`}
+              bgPos={"center"}
+              bgSize={"cover"}
+              bgRepeat={"no-repeat"}
+              height={"200px"}
+              width={"350px"}
+              borderRadius={"10px"}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <Text
+                fontSize={"35px"}
+                fontWeight={500}
+                fontFamily={"CeraRoundPro"}
+                width={"80%"}
+              >
+                Personal Injury{" "}
+              </Text>
+            </GridItem>
+          </Grid>
+          <Button
+            background={"none"}
+            color={"white"}
+            border={"1px solid #FFFFFF"}
+            borderRadius={"8px"}
+            fontSize={"16px"}
+            fontWeight={500}
+            fontFamily={"CeraRoundPro"}
+            _hover={{
+              borderColor: "#beab7c",
+              backgroundColor: "black",
+              color: "#beab7c",
+              transition: "all 0.3s ease",
+            }}
+          >
+            {" "}
+            All Services
+          </Button>
         </Box>
       </section>
 
       <section id="Panels">
-        <Box minHeight="100vh" bg="gray.900" p={10}>
-          <Text color="white" fontSize="2xl">
-            Panels Content
+        <Box
+          width="100%"
+          height="80vh"
+          p={0}
+          m={0}
+          display={"flex"}
+          flexDir={"column"}
+          alignItems={"flex-start"}
+          justifyContent={"center"}
+          gap={"50px"}
+        >
+          <Text
+            color="white"
+            fontSize="25px"
+            fontWeight={300}
+            fontFamily={
+              "HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif"
+            }
+            ml={"12.5%"}
+          >
+            A small intro about what our team is like, diverse etc. just a few
+            lines.{" "}
           </Text>
+          <Box
+            height={"50%"}
+            width={"80%"}
+            backgroundColor={"#145c77"}
+            alignSelf={"center"}
+            borderRadius={"8px"}
+          >
+            <Grid
+              templateColumns={"repeat(4, 1fr)"}
+              templateRows={"repeat(3, 1fr)"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              height={"100%"}
+              width={"100%"}
+            >
+              <GridItem justifySelf={"center"}>
+                <Image
+                  src={PanelOne}
+                  height={"60px"}
+                  width={"150px"}
+                  objectFit={"cover"}
+                />
+              </GridItem>
+              <GridItem justifySelf={"center"}>
+                <Image
+                  src={PanelTwo}
+                  height={"60px"}
+                  width={"150px"}
+                  objectFit={"cover"}
+                />
+              </GridItem>
+              <GridItem justifySelf={"center"}>
+                <Image
+                  src={PanelThree}
+                  height={"60px"}
+                  width={"150px"}
+                  objectFit={"cover"}
+                />
+              </GridItem>
+              <GridItem justifySelf={"center"}>
+                <Image
+                  src={PanelFour}
+                  height={"55px"}
+                  width={"150px"}
+                  objectFit={"cover"}
+                />
+              </GridItem>
+              <GridItem justifySelf={"center"}>
+                <Image
+                  src={PanelOne}
+                  height={"60px"}
+                  width={"150px"}
+                  objectFit={"cover"}
+                />
+              </GridItem>
+              <GridItem justifySelf={"center"}>
+                <Image
+                  src={PanelTwo}
+                  height={"60px"}
+                  width={"150px"}
+                  objectFit={"cover"}
+                />
+              </GridItem>
+              <GridItem justifySelf={"center"}>
+                <Image
+                  src={PanelThree}
+                  height={"60px"}
+                  width={"150px"}
+                  objectFit={"cover"}
+                />
+              </GridItem>
+              <GridItem justifySelf={"center"}>
+                <Image
+                  src={PanelFour}
+                  height={"55px"}
+                  width={"150px"}
+                  objectFit={"cover"}
+                />
+              </GridItem>
+              <GridItem justifySelf={"center"}>
+                <Image
+                  src={PanelOne}
+                  height={"60px"}
+                  width={"150px"}
+                  objectFit={"cover"}
+                />
+              </GridItem>
+              <GridItem justifySelf={"center"}>
+                <Image
+                  src={PanelTwo}
+                  height={"60px"}
+                  width={"150px"}
+                  objectFit={"cover"}
+                />
+              </GridItem>
+              <GridItem justifySelf={"center"}>
+                <Image
+                  src={PanelThree}
+                  height={"60px"}
+                  width={"150px"}
+                  objectFit={"cover"}
+                />
+              </GridItem>
+              <GridItem justifySelf={"center"}>
+                <Image
+                  src={PanelFour}
+                  height={"55px"}
+                  width={"150px"}
+                  objectFit={"cover"}
+                />
+              </GridItem>
+            </Grid>
+          </Box>
         </Box>
       </section>
 
-      <section id="News">
-        <Box minHeight="100vh" bg="gray.800" p={10}>
+      {/* <section id="News">
+        <Box minHeight="100vh" p={10}>
           <Text color="white" fontSize="2xl">
             News Content
           </Text>
@@ -191,12 +553,12 @@ const LandingPage = () => {
       </section>
 
       <section id="Insights">
-        <Box minHeight="100vh" bg="gray.800" p={10}>
+        <Box minHeight="100vh" p={10}>
           <Text color="white" fontSize="2xl">
             Insights Content
           </Text>
         </Box>
-      </section>
+      </section> */}
       <section id="none">
         <Footer />
       </section>
