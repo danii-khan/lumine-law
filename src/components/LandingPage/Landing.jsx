8da18462b3ba0b7d81c1953a6283946ce9bd4fdb
daniikhan23 <daniyal.ashraf.2021@gmail.com>
2024-11-22 02:05:23 +0500

    Updated landing page to new design

--- src/components/LandingPage/Landing.jsx
+++ src/components/LandingPage/Landing.jsx
@@ -12,18 +12,19 @@ import {
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 import AnimatedPanels from "./AnimatedPanels";
-import MainBg from "../../assets/landing/main-bg.jpg";
+import MainBg from "../../assets/landing/main-bg.png";
 import AboutBg from "../../assets/landing/about-bg.png";
-import ServicesBgOne from "../../assets/landing/services-bg-one.png";
-import ServicesBgTwo from "../../assets/landing/services-bg-two.png";
-import ServicesBgThree from "../../assets/landing/services-bg-three.png";
-import ServicesBgFour from "../../assets/landing/services-bg-four.png";
-import ServicesBgFive from "../../assets/landing/services-bg-five.png";
-import ServicesBgSix from "../../assets/landing/services-bg-six.png";
+import ServicesBg from "../../assets/landing/services.png";
+import PanelsBg from "../../assets/landing/panels.png";
 import PanelOne from "../../assets/landing/panel-one.png";
 import PanelTwo from "../../assets/landing/panel-two.png";
 import PanelThree from "../../assets/landing/panel-three.png";
 import PanelFour from "../../assets/landing/panel-four.png";
+import NewsInsightsBg from "../../assets/landing/news-insights.png";
+import NewsInsightsOne from "../../assets/landing/news-insights-one.png";
+import NewsInsightsTwo from "../../assets/landing/news-insights-two.png";
+import NewsInsightsThree from "../../assets/landing/news-insights-three.png";
+import NewsInsightsFour from "../../assets/landing/news-insights-four.png";
 import "../../styles/fonts.css";
 import { useEffect, useState } from "react";
 
@@ -77,7 +78,7 @@ const ScrollSpySidebar = () => {
       pointerEvents="auto"
     >
       {activeSection !== "none" &&
-        ["Who-we-are", "Our-services", "Panels", "News", "Insights"].map(
+        ["Who-we-are", "Our-Services", "Panels", "News-and-Insights"].map(
           (id) => (
             <>
               <Button
@@ -86,8 +87,11 @@ const ScrollSpySidebar = () => {
                 variant="ghost"
                 onClick={() => scrollToSection(id)}
                 background={"none"}
-                color={"white"}
-                _hover={{ color: "#beab7c", opacity: "100%" }}
+                color={activeSection === "Panels" ? "black" : "white"}
+                _hover={{
+                  color: activeSection !== "Panels" && "#beab7c",
+                  opacity: "100%",
+                }}
                 // fontFamily="CeraRoundPro"
                 fontWeight={400}
                 fontStyle={"italic"}
@@ -98,7 +102,8 @@ const ScrollSpySidebar = () => {
               >
                 <span
                   style={{
-                    border: "1px solid white",
+                    border: "1px solid",
+                    borderColor: activeSection === "Panels" ? "black" : "white",
                     width: "50px",
                     opacity: `${activeSection === id ? "100%" : "25%"}`,
                     mb: `${activeSection === id ? "100px" : "0"}`,
@@ -124,14 +129,6 @@ const LandingPage = () => {
       overflow="auto"
       m={0}
       p={0}
-      background="linear-gradient(0deg, 
-    #000000 12.5%, 
-    #124F66 35.68%, 
-    #249ECB 47.23%, 
-    #124F66 58.12%, 
-    #1B7798 67%, 
-    #000000 93%
-  )"
       css={{
         scrollSnapType: "y mandatory",
         "&::-webkit-scrollbar": {
@@ -287,7 +284,7 @@ const LandingPage = () => {
 
       {/* Services content */}
       <section
-        id="Our-services"
+        id="Our-Services"
         style={{
           scrollSnapAlign: "start",
           scrollSnapStop: "always",
@@ -305,142 +302,237 @@ const LandingPage = () => {
           alignItems={"center"}
           justifyContent={"center"}
           gap={"50px"}
+          bgImage={`url(${ServicesBg})`}
+          bgPos={"center"}
+          bgSize={"cover"}
+          bgRepeat={"no-repeat"}
         >
           <Grid
-            templateColumns="repeat(3, 1fr)"
+            templateColumns="repeat(4, 1fr)"
             templateRows={"repeat(2, 1fr)"}
             gap="10"
           >
+            {/* 1 */}
             <GridItem
-              bgImage={`url(${ServicesBgOne})`}
-              bgPos={"center"}
-              bgSize={"cover"}
-              bgRepeat={"no-repeat"}
+              border={"1px solid white"}
+              backgroundColor={"transparent"}
               height={"200px"}
-              width={"350px"}
+              width={"250px"}
               borderRadius={"10px"}
               display={"flex"}
               justifyContent={"center"}
               alignItems={"center"}
               cursor={"pointer"}
+              _hover={{
+                backgroundColor: "black",
+                borderColor: "#beab7c",
+                color: "#beab7c",
+              }}
+              transition={"all 0.3s ease"}
             >
               <Text
-                fontSize={"35px"}
-                fontWeight={500}
+                fontSize={"30px"}
+                fontWeight={400}
                 fontFamily={"CeraRoundPro"}
                 width={"80%"}
               >
                 Immigration Services
               </Text>
             </GridItem>
+
+            {/* 2 */}
             <GridItem
-              bgImage={`url(${ServicesBgTwo})`}
-              bgPos={"center"}
-              bgSize={"cover"}
-              bgRepeat={"no-repeat"}
+              border={"1px solid white"}
+              backgroundColor={"transparent"}
               height={"200px"}
-              width={"350px"}
+              width={"250px"}
               borderRadius={"10px"}
               display={"flex"}
               justifyContent={"center"}
               alignItems={"center"}
               cursor={"pointer"}
+              _hover={{
+                backgroundColor: "black",
+                borderColor: "#beab7c",
+                color: "#beab7c",
+              }}
+              transition={"all 0.3s ease"}
             >
               <Text
-                fontSize={"35px"}
-                fontWeight={500}
+                fontSize={"30px"}
+                fontWeight={400}
                 fontFamily={"CeraRoundPro"}
                 width={"80%"}
               >
                 Residential and Commercial Property{" "}
               </Text>
             </GridItem>
+
+            {/* 3 */}
             <GridItem
-              bgImage={`url(${ServicesBgThree})`}
-              bgPos={"center"}
-              bgSize={"cover"}
-              bgRepeat={"no-repeat"}
+              border={"1px solid white"}
+              backgroundColor={"transparent"}
               height={"200px"}
-              width={"350px"}
+              width={"250px"}
               borderRadius={"10px"}
               display={"flex"}
               justifyContent={"center"}
               alignItems={"center"}
               cursor={"pointer"}
+              _hover={{
+                backgroundColor: "black",
+                borderColor: "#beab7c",
+                color: "#beab7c",
+              }}
+              transition={"all 0.3s ease"}
             >
               <Text
-                fontSize={"35px"}
-                fontWeight={500}
+                fontSize={"30px"}
+                fontWeight={400}
                 fontFamily={"CeraRoundPro"}
                 width={"80%"}
               >
                 Dispute Resolution and Civil Litigation{" "}
               </Text>
             </GridItem>
+
+            {/* 4 */}
             <GridItem
-              bgImage={`url(${ServicesBgFour})`}
-              bgPos={"center"}
-              bgSize={"cover"}
-              bgRepeat={"no-repeat"}
+              border={"1px solid white"}
+              backgroundColor={"transparent"}
               height={"200px"}
-              width={"350px"}
+              width={"250px"}
               borderRadius={"10px"}
               display={"flex"}
               justifyContent={"center"}
               alignItems={"center"}
               cursor={"pointer"}
+              _hover={{
+                backgroundColor: "black",
+                borderColor: "#beab7c",
+                color: "#beab7c",
+              }}
+              transition={"all 0.3s ease"}
             >
               <Text
-                fontSize={"35px"}
-                fontWeight={500}
+                fontSize={"30px"}
+                fontWeight={400}
                 fontFamily={"CeraRoundPro"}
                 width={"80%"}
               >
                 Landlord and Tenant Disputes{" "}
               </Text>
             </GridItem>
+
+            {/* 5 */}
             <GridItem
-              bgImage={`url(${ServicesBgFive})`}
-              bgPos={"center"}
-              bgSize={"cover"}
-              bgRepeat={"no-repeat"}
+              border={"1px solid white"}
+              backgroundColor={"transparent"}
               height={"200px"}
-              width={"350px"}
+              width={"250px"}
               borderRadius={"10px"}
               display={"flex"}
               justifyContent={"center"}
               alignItems={"center"}
               cursor={"pointer"}
+              _hover={{
+                backgroundColor: "black",
+                borderColor: "#beab7c",
+                color: "#beab7c",
+              }}
+              transition={"all 0.3s ease"}
             >
               <Text
-                fontSize={"35px"}
-                fontWeight={500}
+                fontSize={"30px"}
+                fontWeight={400}
                 fontFamily={"CeraRoundPro"}
                 width={"80%"}
               >
                 Family and Children{" "}
               </Text>
             </GridItem>
+
+            {/* 6 */}
             <GridItem
-              bgImage={`url(${ServicesBgSix})`}
-              bgPos={"center"}
-              bgSize={"cover"}
-              bgRepeat={"no-repeat"}
+              border={"1px solid white"}
+              backgroundColor={"transparent"}
               height={"200px"}
-              width={"350px"}
+              width={"250px"}
               borderRadius={"10px"}
               display={"flex"}
               justifyContent={"center"}
               alignItems={"center"}
               cursor={"pointer"}
+              _hover={{
+                backgroundColor: "black",
+                borderColor: "#beab7c",
+                color: "#beab7c",
+              }}
+              transition={"all 0.3s ease"}
             >
               <Text
-                fontSize={"35px"}
-                fontWeight={500}
+                fontSize={"30px"}
+                fontWeight={400}
                 fontFamily={"CeraRoundPro"}
                 width={"80%"}
               >
                 Personal Injury{" "}
+              </Text>
+            </GridItem>
+
+            {/* 7 */}
+            <GridItem
+              border={"1px solid white"}
+              backgroundColor={"transparent"}
+              height={"200px"}
+              width={"250px"}
+              borderRadius={"10px"}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              cursor={"pointer"}
+              _hover={{
+                backgroundColor: "black",
+                borderColor: "#beab7c",
+                color: "#beab7c",
+              }}
+              transition={"all 0.3s ease"}
+            >
+              <Text
+                fontSize={"30px"}
+                fontWeight={400}
+                fontFamily={"CeraRoundPro"}
+                width={"80%"}
+              >
+                New Service{" "}
+              </Text>
+            </GridItem>
+
+            {/* 8 */}
+            <GridItem
+              border={"1px solid white"}
+              backgroundColor={"transparent"}
+              height={"200px"}
+              width={"250px"}
+              borderRadius={"10px"}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              cursor={"pointer"}
+              _hover={{
+                backgroundColor: "black",
+                borderColor: "#beab7c",
+                color: "#beab7c",
+              }}
+              transition={"all 0.3s ease"}
+            >
+              <Text
+                fontSize={"30px"}
+                fontWeight={400}
+                fontFamily={"CeraRoundPro"}
+                width={"80%"}
+              >
+                All Services{" "}
               </Text>
             </GridItem>
           </Grid>
@@ -484,9 +576,13 @@ const LandingPage = () => {
           alignItems={"flex-start"}
           justifyContent={"center"}
           gap={"50px"}
+          bgImage={`url(${PanelsBg})`}
+          bgPos={"center"}
+          bgSize={"cover"}
+          bgRepeat={"no-repeat"}
         >
           <Text
-            color="white"
+            color="black"
             fontSize="25px"
             fontWeight={300}
             fontFamily={
@@ -500,6 +596,76 @@ const LandingPage = () => {
           <AnimatedPanels
             images={[PanelOne, PanelTwo, PanelThree, PanelFour]}
           />
+        </Box>
+      </section>
+
+      {/* News and Insights content */}
+      <section
+        id="News-and-Insights"
+        style={{
+          scrollSnapAlign: "start",
+          scrollSnapStop: "always",
+          height: "100vh",
+        }}
+      >
+        <Box
+          width="100%"
+          height="100%"
+          p={0}
+          m={0}
+          display={"flex"}
+          flexDir={"column"}
+          alignItems={"flex-start"}
+          justifyContent={"center"}
+          gap={"50px"}
+          bgImage={`url(${NewsInsightsBg})`}
+          bgPos={"center"}
+          bgSize={"cover"}
+          bgRepeat={"no-repeat"}
+        >
+          <Text
+            color="white"
+            fontSize="25px"
+            fontWeight={300}
+            fontFamily={
+              "HelveticaNeue-Light, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif"
+            }
+            ml={"12.5%"}
+          >
+            A small intro about what our team is like, diverse etc. just a few
+            lines.{" "}
+          </Text>
+          <HStack
+            width={"100%"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            gap={"50px"}
+          >
+            <Image
+              src={NewsInsightsOne}
+              height={"100%"}
+              width={"20%"}
+              objectFit={"cover"}
+            />
+            <Image
+              src={NewsInsightsTwo}
+              height={"100%"}
+              width={"20%"}
+              objectFit={"cover"}
+            />
+            <Image
+              src={NewsInsightsThree}
+              height={"100%"}
+              width={"20%"}
+              objectFit={"cover"}
+            />
+            <Image
+              src={NewsInsightsFour}
+              height={"100%"}
+              width={"20%"}
+              objectFit={"cover"}
+            />
+          </HStack>
         </Box>
       </section>
 
