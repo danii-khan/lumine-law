a72d99cc4e09e69688a2cc0f61255703f224f302
daniikhan23 <daniyal.ashraf.2021@gmail.com>
2024-11-28 04:01:58 +0500

    Reverting some changes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route
-            path="/lumine-law/"
+            path="/"
             element={
               <PageTransition>
                 <LandingPage />
