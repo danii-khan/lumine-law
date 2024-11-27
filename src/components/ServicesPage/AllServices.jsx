268919d63e3750bb5cfbdc9b4046e7e9c6b4f200
daniikhan23 <daniyal.ashraf.2021@gmail.com>
2024-11-28 01:01:24 +0500

    Made all ServicesPage responsive to different screen sizes

--- src/components/ServicesPage/AllServices.jsx
+++ src/components/ServicesPage/AllServices.jsx
@@ -39,7 +39,12 @@ const AllServices = () => {
           {/* Banner */}
           <Box
             width={"100%"}
-            height={"200px"}
+            height={{
+              xs: "75px",
+              lg: "100px",
+              xl: "125px",
+              "2xl": "150px",
+            }}
             bgImage={`url(${BannerImg})`}
             bgSize={"cover"}
             bgPos={"center"}
@@ -49,14 +54,14 @@ const AllServices = () => {
             alignItems={"center"}
           >
             <Text
-              fontSize={"45px"}
+              fontSize={{ xs: "20px", lg: "30px", xl: "36px", "2xl": "45px" }}
               fontWeight={600}
               fontFamily={"CeraRoundPro"}
               fontStyle={"italic"}
             >
               Services
             </Text>
-            <Box width={"60%"}>{/* Empty */}</Box>
+            <Box width={{ xs: "60%", md: "65%", lg: "75%" }}>{/* Empty */}</Box>
           </Box>
 
           {/* Main */}
@@ -70,24 +75,38 @@ const AllServices = () => {
             <HStack
               justifyContent={"space-around"}
               alignItems={"center"}
-              my={"2.5%"}
+              my={{ xs: "5%", lg: "2%", xl: "2%", "2xl": "1%" }}
             >
               <Text
                 fontFamily={"CeraRoundPro"}
-                fontSize={"35px"}
+                fontSize={{ xs: "14px", lg: "20px", xl: "26px", "2xl": "30px" }}
                 fontWeight={500}
               >
                 Our Legal Services
               </Text>
-              <Box width={"70%"}>{/* Empty */}</Box>
+              <Box
+                width={{
+                  xs: "55%",
+                  md: "60%",
+                  lg: "62.5%",
+                  xl: "65%",
+                  "2xl": "70%",
+                }}
+              >
+                {/* Empty */}
+              </Box>
             </HStack>
 
             {/* Main Para */}
-            <Box width={"95%"} alignSelf={"center"} mb={"5%"}>
+            <Box
+              width={{ xs: "95%", lg: "92.5%", xl: "95%" }}
+              alignSelf={"center"}
+              mb={{ xs: "5%", lg: "2.5%", xl: "5%" }}
+            >
               <Text
                 fontFamily={"CeraRoundPro"}
                 fontWeight={400}
-                fontSize={"20px"}
+                fontSize={{ xs: "10px", lg: "12px", xl: "14px", "2xl": "16px" }}
                 textAlign={"left"}
               >
                 Our legal services are highly recommended by our clients and
@@ -110,25 +129,35 @@ const AllServices = () => {
             flexFlow={"column"}
             alignItems={"center"}
             color={"black"}
-            mb="50px"
+            mb={{ xs: "25px", lg: "50px" }}
           >
             <Grid
-              width={"70%"}
-              templateColumns={"repeat(3, 1fr)"}
-              templateRows={"repeat(3, 1fr)"}
-              gap={"50px"}
+              width={{ xs: "95%", lg: "95%", xl: "95%", "2xl": "80%" }}
+              templateColumns={{ xs: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }}
+              templateRows={{ xs: "repeat(4, 1fr)", lg: "repeat(3, 1fr)" }}
+              gap={{ xs: "10px", lg: "20px", xl: "25px", "2xl": "30px" }}
               justifyItems={"center"}
               alignItems={"center"}
             >
               {/* Immigration */}
               <GridItem
-                width={"100%"}
-                height={"300px"}
+                width={{ xs: "100%", lg: "100%", xl: "100%", "2xl": "90%" }}
+                height={{
+                  xs: "125px",
+                  lg: "150px",
+                  xl: "175px",
+                  "2xl": "200px",
+                }}
+                padding={{
+                  xs: "4px 10px",
+                  lg: "6px 14px",
+                  xl: "8px 16px",
+                  "2xl": "10px 20px",
+                }}
                 bgImage={`url(${ImmigrationImg})`}
                 bgSize={"cover"}
                 bgPos={"center"}
                 bgRepeat={"no-repeat"}
-                padding={"10px 20px"}
                 border={"1px solid black"}
                 borderRadius={"8px"}
                 cursor={"pointer"}
@@ -153,7 +182,12 @@ const AllServices = () => {
                     gap={"10px"}
                   >
                     <Text
-                      fontSize={"24px"}
+                      fontSize={{
+                        xs: "11px",
+                        lg: "14px",
+                        xl: "16px",
+                        "2xl": "20px",
+                      }}
                       fontFamily={"CeraRoundPro"}
                       fontWeight={500}
                       textAlign={"left"}
@@ -167,7 +201,12 @@ const AllServices = () => {
                       alignItems={"flex-start"}
                     >
                       <Text
-                        fontSize={"18px"}
+                        fontSize={{
+                          xs: "10px",
+                          lg: "12px",
+                          xl: "13px",
+                          "2xl": "16px",
+                        }}
                         fontFamily={"CeraRoundPro"}
                         fontWeight={400}
                       >
@@ -180,9 +219,19 @@ const AllServices = () => {
               </GridItem>
               {/* Residential + Commercial */}
               <GridItem
-                width={"100%"}
-                height={"300px"}
-                padding={"10px 20px"}
+                width={{ xs: "100%", lg: "100%", xl: "100%", "2xl": "90%" }}
+                height={{
+                  xs: "125px",
+                  lg: "150px",
+                  xl: "175px",
+                  "2xl": "200px",
+                }}
+                padding={{
+                  xs: "4px 10px",
+                  lg: "6px 14px",
+                  xl: "8px 16px",
+                  "2xl": "10px 20px",
+                }}
                 border={"1px solid black"}
                 borderRadius={"8px"}
                 cursor={"pointer"}
@@ -201,7 +250,12 @@ const AllServices = () => {
                   gap={"10px"}
                 >
                   <Text
-                    fontSize={"24px"}
+                    fontSize={{
+                      xs: "11px",
+                      lg: "14px",
+                      xl: "16px",
+                      "2xl": "20px",
+                    }}
                     fontFamily={"CeraRoundPro"}
                     fontWeight={500}
                     textAlign={"left"}
@@ -215,7 +269,12 @@ const AllServices = () => {
                     alignItems={"flex-start"}
                   >
                     <Text
-                      fontSize={"18px"}
+                      fontSize={{
+                        xs: "10px",
+                        lg: "12px",
+                        xl: "13px",
+                        "2xl": "16px",
+                      }}
                       fontFamily={"CeraRoundPro"}
                       fontWeight={400}
                     >
@@ -227,9 +286,19 @@ const AllServices = () => {
               </GridItem>
               {/* Dispute Resolution */}
               <GridItem
-                width={"100%"}
-                height={"300px"}
-                padding={"10px 20px"}
+                width={{ xs: "100%", lg: "100%", xl: "100%", "2xl": "90%" }}
+                height={{
+                  xs: "125px",
+                  lg: "150px",
+                  xl: "175px",
+                  "2xl": "200px",
+                }}
+                padding={{
+                  xs: "4px 10px",
+                  lg: "6px 14px",
+                  xl: "8px 16px",
+                  "2xl": "10px 20px",
+                }}
                 border={"1px solid black"}
                 borderRadius={"8px"}
                 cursor={"pointer"}
@@ -248,7 +317,12 @@ const AllServices = () => {
                   gap={"10px"}
                 >
                   <Text
-                    fontSize={"24px"}
+                    fontSize={{
+                      xs: "11px",
+                      lg: "14px",
+                      xl: "16px",
+                      "2xl": "20px",
+                    }}
                     fontFamily={"CeraRoundPro"}
                     fontWeight={500}
                     textAlign={"left"}
@@ -262,7 +336,12 @@ const AllServices = () => {
                     alignItems={"flex-start"}
                   >
                     <Text
-                      fontSize={"18px"}
+                      fontSize={{
+                        xs: "10px",
+                        lg: "12px",
+                        xl: "13px",
+                        "2xl": "16px",
+                      }}
                       fontFamily={"CeraRoundPro"}
                       fontWeight={400}
                     >
@@ -274,9 +353,19 @@ const AllServices = () => {
               </GridItem>
               {/* Landlord + Tenant */}
               <GridItem
-                width={"100%"}
-                height={"300px"}
-                padding={"10px 20px"}
+                width={{ xs: "100%", lg: "100%", xl: "100%", "2xl": "90%" }}
+                height={{
+                  xs: "125px",
+                  lg: "150px",
+                  xl: "175px",
+                  "2xl": "200px",
+                }}
+                padding={{
+                  xs: "4px 10px",
+                  lg: "6px 14px",
+                  xl: "8px 16px",
+                  "2xl": "10px 20px",
+                }}
                 border={"1px solid black"}
                 borderRadius={"8px"}
                 cursor={"pointer"}
@@ -295,7 +384,12 @@ const AllServices = () => {
                   gap={"10px"}
                 >
                   <Text
-                    fontSize={"24px"}
+                    fontSize={{
+                      xs: "11px",
+                      lg: "14px",
+                      xl: "16px",
+                      "2xl": "20px",
+                    }}
                     fontFamily={"CeraRoundPro"}
                     fontWeight={500}
                     textAlign={"left"}
@@ -309,7 +403,12 @@ const AllServices = () => {
                     alignItems={"flex-start"}
                   >
                     <Text
-                      fontSize={"18px"}
+                      fontSize={{
+                        xs: "10px",
+                        lg: "12px",
+                        xl: "13px",
+                        "2xl": "16px",
+                      }}
                       fontFamily={"CeraRoundPro"}
                       fontWeight={400}
                     >
@@ -321,9 +420,19 @@ const AllServices = () => {
               </GridItem>
               {/* Family + Children */}
               <GridItem
-                width={"100%"}
-                height={"300px"}
-                padding={"10px 20px"}
+                width={{ xs: "100%", lg: "100%", xl: "100%", "2xl": "90%" }}
+                height={{
+                  xs: "125px",
+                  lg: "150px",
+                  xl: "175px",
+                  "2xl": "200px",
+                }}
+                padding={{
+                  xs: "4px 10px",
+                  lg: "6px 14px",
+                  xl: "8px 16px",
+                  "2xl": "10px 20px",
+                }}
                 border={"1px solid black"}
                 borderRadius={"8px"}
                 cursor={"pointer"}
@@ -342,7 +451,12 @@ const AllServices = () => {
                   gap={"10px"}
                 >
                   <Text
-                    fontSize={"24px"}
+                    fontSize={{
+                      xs: "11px",
+                      lg: "14px",
+                      xl: "16px",
+                      "2xl": "20px",
+                    }}
                     fontFamily={"CeraRoundPro"}
                     fontWeight={500}
                     textAlign={"left"}
@@ -356,7 +470,12 @@ const AllServices = () => {
                     alignItems={"flex-start"}
                   >
                     <Text
-                      fontSize={"18px"}
+                      fontSize={{
+                        xs: "10px",
+                        lg: "12px",
+                        xl: "13px",
+                        "2xl": "16px",
+                      }}
                       fontFamily={"CeraRoundPro"}
                       fontWeight={400}
                     >
@@ -368,9 +487,19 @@ const AllServices = () => {
               </GridItem>
               {/* New Service */}
               <GridItem
-                width={"100%"}
-                height={"300px"}
-                padding={"10px 20px"}
+                width={{ xs: "100%", lg: "100%", xl: "100%", "2xl": "90%" }}
+                height={{
+                  xs: "125px",
+                  lg: "150px",
+                  xl: "175px",
+                  "2xl": "200px",
+                }}
+                padding={{
+                  xs: "4px 10px",
+                  lg: "6px 14px",
+                  xl: "8px 16px",
+                  "2xl": "10px 20px",
+                }}
                 border={"1px solid black"}
                 borderRadius={"8px"}
                 cursor={"pointer"}
@@ -389,7 +518,12 @@ const AllServices = () => {
                   gap={"10px"}
                 >
                   <Text
-                    fontSize={"24px"}
+                    fontSize={{
+                      xs: "11px",
+                      lg: "14px",
+                      xl: "16px",
+                      "2xl": "20px",
+                    }}
                     fontFamily={"CeraRoundPro"}
                     fontWeight={500}
                     textAlign={"left"}
@@ -403,7 +537,12 @@ const AllServices = () => {
                     alignItems={"flex-start"}
                   >
                     <Text
-                      fontSize={"18px"}
+                      fontSize={{
+                        xs: "10px",
+                        lg: "12px",
+                        xl: "13px",
+                        "2xl": "16px",
+                      }}
                       fontFamily={"CeraRoundPro"}
                       fontWeight={400}
                     >
@@ -414,12 +553,25 @@ const AllServices = () => {
                 </Box>
               </GridItem>
               {/* empty grid */}
-              <GridItem>{/* empty */}</GridItem>
+              <GridItem display={{ xs: "none", lg: "grid" }}>
+                {/* empty */}
+              </GridItem>
               {/* New Service*/}
               <GridItem
-                width={"100%"}
-                height={"300px"}
-                padding={"10px 20px"}
+                colSpan={{ xs: "2", lg: "1" }}
+                width={{ xs: "100%", lg: "100%", xl: "100%", "2xl": "90%" }}
+                height={{
+                  xs: "125px",
+                  lg: "150px",
+                  xl: "175px",
+                  "2xl": "200px",
+                }}
+                padding={{
+                  xs: "4px 10px",
+                  lg: "6px 14px",
+                  xl: "8px 16px",
+                  "2xl": "10px 20px",
+                }}
                 border={"1px solid black"}
                 borderRadius={"8px"}
                 cursor={"pointer"}
@@ -438,7 +590,12 @@ const AllServices = () => {
                   gap={"10px"}
                 >
                   <Text
-                    fontSize={"24px"}
+                    fontSize={{
+                      xs: "11px",
+                      lg: "14px",
+                      xl: "16px",
+                      "2xl": "20px",
+                    }}
                     fontFamily={"CeraRoundPro"}
                     fontWeight={500}
                     textAlign={"left"}
@@ -452,7 +609,12 @@ const AllServices = () => {
                     alignItems={"flex-start"}
                   >
                     <Text
-                      fontSize={"18px"}
+                      fontSize={{
+                        xs: "10px",
+                        lg: "12px",
+                        xl: "13px",
+                        "2xl": "16px",
+                      }}
                       fontFamily={"CeraRoundPro"}
                       fontWeight={400}
                     >
