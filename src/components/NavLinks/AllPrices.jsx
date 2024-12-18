34dcff725c43824c1292a3af8582c58486ad3925
daniikhan23 <daniyal.ashraf.2021@gmail.com>
2024-12-18 22:11:08 +0500

    fix: made all prices page more efficient

--- src/components/NavLinks/AllPrices.jsx
+++ src/components/NavLinks/AllPrices.jsx
@@ -6,14 +6,89 @@ import ImmigrationImg from "../../assets/pricing/immigration.png";
 import PropertyImg from "../../assets/pricing/property-prices.png";
 import DebtImg from "../../assets/pricing/debt-recovery.png";
 
+// Price card data
+const priceCards = [
+  {
+    title: "Property Prices",
+    path: "/lumine-law/prices/property-prices",
+    image: PropertyImg,
+  },
+  {
+    title: "Debt Recovery Prices",
+    path: "/lumine-law/prices/debt-recovery",
+    image: DebtImg,
+  },
+  {
+    title: "Immigration Prices",
+    path: "/lumine-law/prices/immigration-prices",
+    image: ImmigrationImg,
+  },
+];
+
+// Reusable price card component
+const PriceCard = ({ item }) => (
+  <GridItem
+    width="100%"
+    height={{
+      xs: "175px",
+      sm: "175px",
+      md: "175px",
+      lg: "200px",
+      xl: "225px",
+      "2xl": "250px",
+    }}
+    padding={{ xs: "10px 10px", lg: "10px 20px" }}
+    border="1px solid black"
+    borderRadius="8px"
+    cursor="pointer"
+    _hover={{
+      opacity: "0.8",
+      transition: "all 0.5s ease",
+    }}
+    bgImage={`url(${item.image})`}
+    bgPos="center"
+    bgSize="cover"
+    bgRepeat="no-repeat"
+  >
+    <Link to={item.path} style={{ textDecoration: "none" }}>
+      <Box
+        height="100%"
+        display="flex"
+        flexFlow="column"
+        justifyContent="space-evenly"
+        alignItems="flex-start"
+        gap="10px"
+      >
+        <Text
+          fontSize={{
+            xs: "14px",
+            sm: "14px",
+            md: "16px",
+            lg: "18px",
+            xl: "20px",
+            "2xl": "22px",
+          }}
+          fontFamily="CeraRoundPro"
+          fontWeight={500}
+          textAlign="left"
+          color="#ffffff"
+        >
+          {item.title}
+        </Text>
+        <HiOutlineArrowLongRight color="#ffffff" size="2em" />
+      </Box>
+    </Link>
+  </GridItem>
+);
+
 const AllPrices = () => {
   return (
-    <Box mb="50px" _loading={"lazy"}>
+    <Box mb="50px" _loading="lazy">
       {/* Header */}
-      <Box width={"100%"} display={"flex"} flexFlow={"column"} color={"black"}>
-        <Box width={"90%"} alignSelf={"center"}>
+      <Box width="100%" display="flex" flexFlow="column" color="black">
+        <Box width="90%" alignSelf="center">
           <Text
-            color={"black"}
+            color="black"
             fontSize={{
               xs: "20px",
               sm: "20px",
@@ -23,7 +98,7 @@ const AllPrices = () => {
               "2xl": "30px",
             }}
             fontWeight={500}
-            fontStyle={"italic"}
+            fontStyle="italic"
             mb={{
               xs: "25px",
               sm: "30px",
@@ -32,7 +107,7 @@ const AllPrices = () => {
               xl: "45px",
               "2xl": "50px",
             }}
-            textAlign={"left"}
+            textAlign="left"
           >
             Pricing
           </Text>
@@ -41,14 +116,14 @@ const AllPrices = () => {
 
       {/* List */}
       <Box
-        width={"100%"}
-        display={"flex"}
-        flexFlow={"column"}
-        alignItems={"center"}
-        color={"black"}
+        width="100%"
+        display="flex"
+        flexFlow="column"
+        alignItems="center"
+        color="black"
       >
         <Grid
-          width={"90%"}
+          width="90%"
           templateColumns={{ xs: "repeat(2, 1fr)", lg: "repeat(3, 1fr)" }}
           gap={{
             xs: "25px",
@@ -58,179 +133,12 @@ const AllPrices = () => {
             xl: "45px",
             "2xl": "50px",
           }}
-          justifyItems={"center"}
-          alignItems={"center"}
+          justifyItems="center"
+          alignItems="center"
         >
-          {/* Col 1 */}
-          <GridItem
-            width={"100%"}
-            height={{
-              xs: "175px",
-              sm: "175px",
-              md: "175px",
-              lg: "200px",
-              xl: "225px",
-              "2xl": "250px",
-            }}
-            padding={{ xs: "10px 10px", lg: "10px 20px" }}
-            border={"1px solid black"}
-            borderRadius={"8px"}
-            cursor={"pointer"}
-            _hover={{
-              opacity: "0.8",
-              transition: "all 0.5s ease",
-            }}
-            bgImage={`url(${PropertyImg})`}
-            bgPos={"center"}
-            bgSize={"cover"}
-            bgRepeat={"no-repeat"}
-          >
-            <Link
-              to={"/lumine-law/prices/property-prices"}
-              style={{ textDecoration: "none" }}
-            >
-              <Box
-                height={"100%"}
-                display={"flex"}
-                flexFlow={"column"}
-                justifyContent={"space-evenly"}
-                alignItems={"flex-start"}
-                gap={"10px"}
-              >
-                <Text
-                  fontSize={{
-                    xs: "14px",
-                    sm: "14px",
-                    md: "16px",
-                    lg: "18px",
-                    xl: "20px",
-                    "2xl": "22px",
-                  }}
-                  fontFamily={"CeraRoundPro"}
-                  fontWeight={500}
-                  textAlign={"left"}
-                  color={"#ffffff"}
-                >
-                  Property Prices
-                </Text>
-                <HiOutlineArrowLongRight color={"#ffffff"} size={"2em"} />
-              </Box>
-            </Link>
-          </GridItem>
-
-          {/* Col 2 */}
-          <GridItem
-            width={"100%"}
-            height={{
-              xs: "175px",
-              sm: "175px",
-              md: "175px",
-              lg: "200px",
-              xl: "225px",
-              "2xl": "250px",
-            }}
-            padding={{ xs: "10px 10px", lg: "10px 20px" }}
-            border={"1px solid black"}
-            borderRadius={"8px"}
-            cursor={"pointer"}
-            _hover={{
-              opacity: "0.8",
-              transition: "all 0.5s ease",
-            }}
-            bgImage={`url(${DebtImg})`}
-            bgPos={"center"}
-            bgSize={"cover"}
-            bgRepeat={"no-repeat"}
-          >
-            <Link
-              to={"/lumine-law/prices/debt-recovery"}
-              style={{ textDecoration: "none" }}
-            >
-              <Box
-                height={"100%"}
-                display={"flex"}
-                flexFlow={"column"}
-                justifyContent={"space-evenly"}
-                alignItems={"flex-start"}
-                gap={"10px"}
-              >
-                <Text
-                  fontSize={{
-                    xs: "14px",
-                    sm: "14px",
-                    md: "16px",
-                    lg: "18px",
-                    xl: "20px",
-                    "2xl": "22px",
-                  }}
-                  fontFamily={"CeraRoundPro"}
-                  fontWeight={500}
-                  textAlign={"left"}
-                  color={"#ffffff"}
-                >
-                  Debt Recovery Prices
-                </Text>
-                <HiOutlineArrowLongRight color={"#ffffff"} size={"2em"} />
-              </Box>
-            </Link>
-          </GridItem>
-
-          {/* Col 3 */}
-          <GridItem
-            width={"100%"}
-            height={{
-              xs: "175px",
-              sm: "175px",
-              md: "175px",
-              lg: "200px",
-              xl: "225px",
-              "2xl": "250px",
-            }}
-            padding={{ xs: "10px 10px", lg: "10px 20px" }}
-            border={"1px solid black"}
-            borderRadius={"8px"}
-            cursor={"pointer"}
-            _hover={{
-              opacity: "0.8",
-              transition: "all 0.5s ease",
-            }}
-            bgImage={`url(${ImmigrationImg})`}
-            bgPos={"center"}
-            bgSize={"cover"}
-            bgRepeat={"no-repeat"}
-          >
-            <Link
-              to={"/lumine-law/prices/immigration-prices"}
-              style={{ textDecoration: "none" }}
-            >
-              <Box
-                height={"100%"}
-                display={"flex"}
-                flexFlow={"column"}
-                justifyContent={"space-evenly"}
-                alignItems={"flex-start"}
-                gap={"10px"}
-              >
-                <Text
-                  fontSize={{
-                    xs: "14px",
-                    sm: "14px",
-                    md: "16px",
-                    lg: "18px",
-                    xl: "20px",
-                    "2xl": "22px",
-                  }}
-                  fontFamily={"CeraRoundPro"}
-                  fontWeight={500}
-                  textAlign={"left"}
-                  color={"#ffffff"}
-                >
-                  Immigration Prices
-                </Text>
-                <HiOutlineArrowLongRight color={"#ffffff"} size={"2em"} />
-              </Box>
-            </Link>
-          </GridItem>
+          {priceCards.map((card, index) => (
+            <PriceCard key={index} item={card} />
+          ))}
         </Grid>
       </Box>
     </Box>
